b5af8a7753905c33587941e203db3fc4e862b8f7
Carson Bush <hyperdriveguy@gmail.com>
2024-11-06 19:51:50 -0700

    Start some actual logic and organization

---
+++ battle/player_battler/stats.gd
@@ -0,0 +1,17 @@
+extends Node
+
+@export var health: int = 10
+@export var strength: int = 5
+@export var skill: int = 5
+@export var speed: int = 5
+@export var awareness: int = 5
+@export var rizz: int = 5
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
