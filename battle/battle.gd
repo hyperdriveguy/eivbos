e9df6a8c0cb99f48ded06228158d68a8cedf247d
Carson Bush <hyperdriveguy@gmail.com>
2024-11-15 15:09:45 -0700

    modification to turn manager and battler stats

--- battle/battle.gd
+++ battle/battle.gd
@@ -3,15 +3,7 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
-	var battler0 = get_child(0)
-	var battler1 = get_child(1)
-	print('0', battler0.stats.speed)
-	print('1', battler1.stats.speed)
-	if battler1.stats.speed > battler0.stats.speed:
-		move_child(battler1, 0)
-
-
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
