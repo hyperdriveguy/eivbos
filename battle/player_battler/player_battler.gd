0e9b1cdc9f2415ed6d03532db629d4c8bdde6f85
Carson Bush <hyperdriveguy@gmail.com>
2024-11-16 11:57:13 -0700

    actually take turns

--- battle/player_battler/player_battler.gd
+++ battle/player_battler/player_battler.gd
@@ -1,6 +1,8 @@
 extends Node2D
+class_name PlayerBattler
 
 signal send_attack(damage: int)
+signal turn_finished
 
 @export var player_input_id: int
 @export var permanent_stats: battler_stats
@@ -8,7 +10,7 @@ signal send_attack(damage: int)
 @onready var temporary_stats: battler_stats = permanent_stats
 @onready var hp_label: Label = $HPLabel
 
-var is_turn
+var is_turn = false
 var input
 
 # Child called ActionsUI with an HBoxContainer child and three buttons as children
@@ -28,9 +30,12 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	if input.is_action_just_pressed("attack_battle"):
-		send_attack.emit(temporary_stats.strength)
-		Input.start_joy_vibration(player_input_id,0.8,.1,.13)
+	if is_turn:
+		if input.is_action_just_pressed("attack_battle"):
+			is_turn = false
+			send_attack.emit(temporary_stats.strength)
+			Input.start_joy_vibration(player_input_id,0.3,.1,.13)
+			turn_finished.emit()
 
 func _refresh_labels():
 	hp_label.text = "HP: " + str(temporary_stats.health)
--- battle/sync_turn_manager.gd
+++ battle/sync_turn_manager.gd
@@ -1,24 +1,37 @@
 extends Node
 
-
+@onready var _num_battlers: int = len(get_children())
 @onready var _battler0 = get_child(0)
 @onready var _battler1 = get_child(1)
+
+var cur_turn: int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	_battler0.send_attack.connect(_battler0_sent_attack)
+	_battler0.turn_finished.connect(_inc_turn)
 	_battler1.send_attack.connect(_battler1_sent_attack)
+	_battler1.turn_finished.connect(_inc_turn)
 	print('0: ', _battler0.temporary_stats.speed)
 	print('1: ', _battler1.temporary_stats.speed)
 	if _battler1.temporary_stats.speed > _battler0.temporary_stats.speed:
 		move_child(_battler1, 0)
-
-
-
+		_battler1.is_turn = true
+	else:
+		_battler0.is_turn = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _inc_turn():
+	# The battler that sent the signal is responsible for releasing the turn
+	cur_turn = (cur_turn + 1) % (_num_battlers)
+	print("num_battlers: ", _num_battlers)
+	print("cur_turn: ", cur_turn)
+	var cur_battler = get_child(cur_turn)
+	print(cur_battler)
+	cur_battler.is_turn = true
 
 func _battler0_sent_attack(damage: int):
 	print("0 hit 1 for ", damage)
